c3e97c56aadf8e5900fc08ade796475c33cb1205
Luiz Gustavo <luizg0509@gmail.com>
2024-09-01 21:44:04 -0300

    p1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use std::{net::TcpListener, net::TcpStream};
+use std::io::{prelude::*, BufReader};
+
+fn read_req(mut request: TcpStream){
+    let buffer =  BufReader::new(&mut request);
+    let request: Vec<_> = buffer.
+        lines().map(|result| result.unwrap()).take_while(|linha| !linha.is_empty()).collect();
+   println!("{request:?}");
+
+}
+
 fn main() {
-    println!("Hello, world!");
+    let listenert = TcpListener::bind("127.0.0.1:8080").unwrap();
+    for s in listenert.incoming(){
+        let stream = s.unwrap();
+
+        read_req(stream)
+    }
 }
