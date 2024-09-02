a0aefcaa33ae069f58545beb5cb8f9ca4d66ed7c
Luiz Gustavo <lgpss@cin.ufpe.br>
2024-09-02 10:30:17 -0300

    Favicon finished

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
-use std::{fs};
+
+use std::str::Bytes;
+use std::time::Duration;
+use std::{fs, thread};
 use std::{net::TcpListener, net::TcpStream};
 use std::io::{prelude::*, BufReader};
 
@@ -17,11 +20,32 @@ fn code_to_status(code: u32) -> String{
     }
 }
 
-fn file_sender(stream: &mut TcpStream, status: u32, file: &str){
+fn file_sender(stream: &mut TcpStream, status: u32, file: &str) -> Result<(), String>{
+    // println!("requisitando o arquivo {file}");
     let status_line = format!("HTTP/1.1 {} {}", status, code_to_status(status));
-    let content = fs::read_to_string(format!("src/{}", file)).unwrap();
-    let response = format!("{status_line}\r\nContent-Length: {}\r\n\r\n{content}", content.len());
-    stream.write_all(response.as_bytes()).unwrap();
+    if file.ends_with(".html"){
+
+        let content = match fs::read_to_string(format!("src/{}", file)) {
+            Ok(l) => l,
+            Err(_) => return Err(format!("Erro ao pegar arquivo {} que é texto", file))
+        };
+
+        let response = format!("{status_line}\r\nContent-Length: {}\r\n\r\n{content}", content.len());
+        stream.write_all(response.as_bytes()).unwrap();
+
+    } else {
+        let content = match fs::read(format!("src{}", file)){
+            Ok(l) => l,
+            Err(_) => return Err(format!("Erro ao pegar arquivo {} que não é texto", file)) 
+        };
+        let response = format!(
+            "HTTP/1.1 200 OK\r\nContent-Type: image/x-icon\r\nContent-Length: {}\r\n\r\n",
+            content.len()
+        );
+        stream.write(response.as_bytes()).unwrap();
+        stream.write(&content).unwrap();
+    }   
+    Ok(())
 }
 
 fn main() {
@@ -31,8 +55,12 @@ fn main() {
         let request =  read_req(&mut stream);
         let uri: Vec<&str> = request[0].split(" ").collect();
         if uri[0] == "GET" {
+            println!("{:?}", uri);
             let file =  if uri[1] != "/" {uri[1]} else {"index.html"};
-            file_sender(&mut stream, 200, file);
+            let _ = file_sender(&mut stream, 200, file).unwrap();
+            // thread::sleep(Duration::from_secs(7));
         }// fazer um else para outros casos
     }
+    // let s = fs::read("src/favicon.ico").unwrap();
+    // println!("{s:?}");
 }
