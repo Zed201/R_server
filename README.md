653aa96225140ec7d2e31fced27d920397a354f6
Luiz Gustavo <luizg0509@gmail.com>
2024-09-22 21:18:43 -0300

    Cli arguments first test

--- README.md
+++ README.md
@@ -9,8 +9,9 @@ Um servidor http criado a apartir do ultimo projeto do livro oficial de rust, ba
 - [X] Melhorar o threadpool, ou adicionar o tokio, ele não ta pegando no pc de casa(Agora só libera 215 bytes)
 - [X] Fazer 2 "modos" um live server e outro server normal
 - [ ] Melhorar o resto do sistema com o tungstenite
-- [ ] Melhorar os argumentos de cli
+- [X] Melhorar os argumentos de cli
 - [ ] Optimizar uso de memória
 - [ ] Melhorar saida do modo live
 - [ ] Testar com multiplos html e testar com retirada de "marcadores" dos vistos
 - [ ] Fazer alguma logica para atualizar outros arquivos além de html
+- [ ] Verificar o pq quando usa os argumentos do clap ele deixa mais memória leaked
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 #![allow(unused_imports)]
-
+// organizar esses imports
 use std::env;
 use std::io::Write;
 use std::time::Duration;
@@ -31,19 +31,62 @@ use std::io::prelude::*;
 use std::path::*;
 use std::time::SystemTime;
 
+use clap::*;
+
+#[derive(Copy, Clone, Debug, PartialEq, Eq, ValueEnum)]
+enum Mode{
+    Web,
+    Live
+}
+
+static MODE_STR: &str = "Escolha entre os modos web(servidor http normal) e o \nlive(servidor funcionando como live server)\n";
+static PORT_STR: &str = "Escolha da porta na qual o servidor vai ficar ouvindo, \nse for escolhido o modo live, o servidor vai ficar na 'porta' \nescolhida e o websocket vai ficar na 'porta + 1'\n";
+
 fn main() {
-	let args: Vec<String> = env::args().collect();
-	if args.len() < 3 {
-		// fazer um "default" para web depois
-		panic!("Porta não informada");
-	}
-	let porta: u32 = args[1].parse().expect("Erro ao pegar a porta, (!não numérica)");
+        // so peguei do gpt
+        let cmd = Command::new("R_server")
+        .args(&[
+            Arg::new("port_no_flag")
+                .help(PORT_STR)
+                .required(false)
+                .value_parser(clap::value_parser!(u16))
+                .index(1),
+            Arg::new("port")
+                .short('p')
+                .long("port")
+                .help(PORT_STR)
+                .required(false)
+                .value_parser(clap::value_parser!(u16)),
+
+            Arg::new("mode_no_flag")
+                .help(MODE_STR)
+                .required(false)
+                .value_parser(clap::builder::EnumValueParser::<Mode>::new())
+                .index(2),
+            Arg::new("mode")
+                .short('m')
+                .long("mode")
+                .help(MODE_STR)
+                .required(false)
+                .value_parser(clap::builder::EnumValueParser::<Mode>::new())
+
+        ]).get_matches();
+
+        let porta = cmd.get_one::<u16>("port")
+                .or_else(|| cmd.get_one::<u16>("port_no_flag"))
+                .cloned()
+                .unwrap_or(8000);
+        let mode = cmd.get_one::<Mode>("mode")
+                .or_else(|| cmd.get_one::<Mode>("mode_no_flag"))
+                .cloned()
+                .unwrap_or(Mode::Web);
+
 	let ip = format!("0.0.0.0:{}", porta);
 	// 0.0.0.0 para ele se conectar a todas as placas de rede
 	// sejam virtuais ou físicas do sistema
-	// TODO: Melhorar a leitura de comandos, talvez adicionar uma lib para isso
+
 	/* o segundo argumento será o "tipo" de servidor, web normal ou live(com reload)
-	 * O normal	será --web e o outro --live
+	 * O normal será web e o outro live
 	 */
 	on();
 	let running = Arc::new(AtomicBool::new(true));
@@ -53,27 +96,24 @@ fn main() {
 		r.store(false, SeqCst);	
 	});
 
-	// let h = thread::spawn(move || { // para testes de tempo
-	//     thread::sleep(Duration::from_secs(50));
-	//     r.store(false, SeqCst);
-	// });
+	// // let h = thread::spawn(move || { // para testes de tempo
+	// //     thread::sleep(Duration::from_secs(50));
+	// //     r.store(false, SeqCst);
+	// // });
 
 	let lister =
 		Arc::new(TcpListener::bind(ip.clone()).expect("Não conseguiu criar o socket na porta escolhida\n"));
 
 	lister.set_nonblocking(true).unwrap();
 
-	match &args[2].as_str() {
-		&"--live" => {
+	match mode {
+		Mode::Live => {
 			live_server(lister, running, porta);
 		}
-		&"--web" => {
+		Mode::Web => {
 			normal_server(lister, running);
 		}
-		_ => {
-			println!("Tipo não especificado")
-		}
-	}
+    	}
 
 	thread::sleep(Duration::from_secs(1));
 	// h.join();
@@ -115,7 +155,7 @@ fn normal_server(lister: Arc<TcpListener>, running: Arc<AtomicBool>) {
 	// }
 }
 
-fn live_server(lister: Arc<TcpListener>, running: Arc<AtomicBool>, porta: u32) {
+fn live_server(lister: Arc<TcpListener>, running: Arc<AtomicBool>, porta: u16) {
 	let (tx, rx) = channel::<u8>();
 
 	let r = Arc::clone(&running);
